b3894261f83f182addbe54c0d5daff8da2dc5a15
Thales Silva <thales.tas@gmail.com>
2025-05-19 15:57:50 +0100

    added lvgl port configuration
    Signed-off-by: Thales Silva <thales.tas@gmail.com>

---
+++ main/lvgl_port.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "misc/lv_types.h"
+#include "esp_lcd_types.h"
+
+#define LCD_H_RES 240
+#define LCD_V_RES 135
+#define LVGL_TASK_STACK_SIZE 4096
+
+lv_display_t* lvgl_init(esp_lcd_panel_io_handle_t *const io_handle, esp_lcd_panel_handle_t *const panel_handle);
