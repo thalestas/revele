52976b04c7c477a3afadf2bfd07a458b03495354
Thales Silva <thales.tas@gmail.com>
2025-05-19 15:59:53 +0100

    init lcd and configure lvgl port run lvgl benchmark demo
    Signed-off-by: Thales Silva <thales.tas@gmail.com>

--- main/t-display-lvgl.c
+++ main/t-display-lvgl.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include "esp_err.h"
+#include "display.h"
+#include "esp_lcd_types.h"
+#include "lvgl_port.h"
+#include "demos/benchmark/lv_demo_benchmark.h"
+
+static char* TAG = "app_main";
 
 void app_main(void)
 {
+	static esp_lcd_panel_io_handle_t io_handle;
+	static esp_lcd_panel_handle_t panel_handle;
 
+	ESP_ERROR_CHECK(app_lcd_init(&io_handle, &panel_handle));
+	lv_display_t *lvgl_disp = lvgl_init(&io_handle, &panel_handle);
+	lv_demo_benchmark();
 }
