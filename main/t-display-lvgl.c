fb737ea42f6ab6629585fe79259d41fcf77f2e81
Thales Silva <thales.tas@gmail.com>
2025-05-29 16:51:10 +0100

    shows saving screen
    Signed-off-by: Thales Silva <thales.tas@gmail.com>

--- main/t-display-lvgl.c
+++ main/t-display-lvgl.c
@@ -1,10 +1,15 @@
+#include "core/lv_obj.h"
+#include "core/lv_obj_style_gen.h"
 #include "esp_err.h"
 #include "display.h"
 #include "esp_lcd_types.h"
 #include "lvgl_port.h"
-#include "demos/benchmark/lv_demo_benchmark.h"
+#include "esp_lvgl_port.h"
+#include "misc/lv_palette.h"
+#include "savings_screen.h"
 
 static char* TAG = "app_main";
+
 
 void app_main(void)
 {
@@ -13,5 +18,16 @@ void app_main(void)
 
 	ESP_ERROR_CHECK(app_lcd_init(&io_handle, &panel_handle));
 	lv_display_t *lvgl_disp = lvgl_init(&io_handle, &panel_handle);
-	lv_demo_benchmark();
+	
+	//lvgl_port_lock(0);
+	////Set background color
+	//lv_obj_clear_flag(lv_scr_act(), LV_OBJ_FLAG_SCROLLABLE);
+	//lv_obj_set_style_bg_color(lv_scr_act(), lv_palette_darken(LV_PALETTE_BLUE_GREY,4), 0);
+
+	//lvgl_port_unlock();
+	
+	savings_screen(83, 98);
+	vTaskDelay(2000/ portTICK_PERIOD_MS);
+	savings_screen(94, 87);
 }
+
