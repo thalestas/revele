59543b3c63c7476a8708996f22e9c550a7d126b4
Thales Silva <thales.tas@gmail.com>
2025-08-13 11:23:27 +0100

    app_main: remove saving_screen
    Signed-off-by: Thales Silva <thales.tas@gmail.com>

--- main/revele.c
+++ main/revele.c
@@ -1,7 +1,6 @@
 #include "display.h"
 #include "esp_lvgl_port.h"
 #include "lvgl_port.h"
-#include "screens/savings_screen.h"
 #include "screens/status_bar.h"
 #include "sdcard.h"
 #include "wifi_prov.h"
@@ -17,22 +16,6 @@ void app_main(void) {
   // Init
   status_bar_init();
 
-  // Init saving screen data
-  saving_data data = {.rent_status = true,
-                      .car_status = true,
-                      .light_status = false,
-                      .wifi_status = true,
-                      .water_status = false,
-                      .gym_status = true,
-                      .insurance_status = false,
-                      .total_spend = 61,
-                      .general_spend = 84,
-                      .aforro_value = {50, 53, 64, 65, 79},
-                      .trade_value = {1, 5, 14, 19, 23}};
-
-  // Create saving screen
-  lv_obj_t *sav_scr = savings_screen(&data);
-
   // Create wallpaper screen
   lv_obj_t *wallpaper = lv_obj_create(NULL);
   LV_IMAGE_DECLARE(arc_dither);
@@ -43,11 +26,6 @@ void app_main(void) {
 
   // Load screens
   while (1) {
-    lvgl_port_lock(0);
-    lv_scr_load_anim(sav_scr, LV_SCR_LOAD_ANIM_FADE_IN, 2000, 0, false);
-    lvgl_port_unlock();
-    vTaskDelay(6000 / portTICK_PERIOD_MS);
-
     lvgl_port_lock(0);
     lv_scr_load_anim(wallpaper, LV_SCR_LOAD_ANIM_FADE_OUT, 2000, 0, false);
     lvgl_port_unlock();
